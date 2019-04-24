3549f627ea792d98e3c15f127ddeb33335b90112
Minhwan Kim <azurelysium@gmail.com>
2019-04-24 10:57:42 +0900

    implement stats diff computation

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,7 @@ pub struct KeyEvent {
 
 pub type Digraph = (char, char);
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct DigraphStats {
     pub size_samples: usize,
     pub mean: f64,
@@ -124,6 +124,139 @@ impl KeystrokeLogger {
     }
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct KeynomeAuthenticatorDiffParams {
+    pub dispersion: bool,
+    pub min_instances: u32,
+    pub max_comparisons: u32,
+}
+
+pub struct KeynomeAuthenticator<'a, 'b> {
+    pub diff_base: f64,
+    pub multiplier: f64,
+    pub diff_params: &'a KeynomeAuthenticatorDiffParams,
+    pub stats: &'b HashMap<Digraph, DigraphStats>,
+}
+
+impl<'a, 'b> KeynomeAuthenticator<'a, 'b> {
+    pub fn new(diff_base: f64, multiplier: f64, diff_params: &'a KeynomeAuthenticatorDiffParams,
+               stats: &'b HashMap<Digraph, DigraphStats>) -> KeynomeAuthenticator<'a, 'b> {
+        KeynomeAuthenticator { diff_base, multiplier, diff_params, stats }
+    }
+
+    pub fn compute_diff(
+        stats_profile: &HashMap<Digraph, DigraphStats>, stats_sample: &HashMap<Digraph, DigraphStats>,
+        diff_params: &KeynomeAuthenticatorDiffParams) -> f64 {
+        
+        let mut diff: f64 = 0.0;
+        let mut n_comparisons: u32 = 0;
+
+        for (k, v) in stats_profile.iter() {
+            if v.size_samples < diff_params.min_instances as usize {
+                continue;
+            }
+
+            if let Some(vs) = stats_sample.get(k) {
+                diff = diff + ((v.mean - vs.mean).abs() / (if diff_params.dispersion { 0.001 + v.std } else { 1.0 }));
+                n_comparisons = n_comparisons + 1;
+            }
+
+            if n_comparisons >= diff_params.max_comparisons {
+                break;
+            }
+        }
+        diff
+    }
+
+    pub fn compute_diff_base(
+        events: &VecDeque<KeyEvent>, n_profile: usize, n_sample: usize,
+        diff_params: &KeynomeAuthenticatorDiffParams) -> Option<f64> {
+
+        if n_profile > events.len() {
+            return None;
+        } else if n_profile % n_sample != 0 {
+            return None;
+        }
+
+        let vec_events: Vec<&KeyEvent> = events.into_iter().collect();
+        let mut kstr = KeystrokeLogger::new();
+        for ev in &vec_events[(events.len()-n_profile)..] {
+            kstr.add_key_event(KeyEvent { timestamp_ms: ev.timestamp_ms, key: ev.key });
+        }
+        let stats = kstr.compute_digraph_statistics();
+
+        let mut diff_base: f64 = 0.0;
+        for i in 0..n_profile/n_sample {
+            let idx_start = (events.len() - n_profile) + n_sample * i;
+            let idx_end = (events.len() - n_profile) + n_sample * (i + 1);
+
+            let mut kstr_sample = KeystrokeLogger::new();
+            for ev in &vec_events[idx_start..idx_end] {
+                kstr_sample.add_key_event(KeyEvent { timestamp_ms: ev.timestamp_ms, key: ev.key });
+            }
+
+            let stats_sample = kstr_sample.compute_digraph_statistics();
+            diff_base = diff_base + KeynomeAuthenticator::compute_diff(&stats, &stats_sample, diff_params);
+        }
+
+        diff_base = diff_base / (n_profile / n_sample) as f64;
+        Some(diff_base)
+    }
+
+    pub fn authenticate(&self, events: &VecDeque<KeyEvent>) -> bool {
+        true
+    }
+}
+
+pub struct UserProfile {
+    pub n_profile: u32,
+    pub n_sample: u32,
+    pub diff_base: f64,
+    pub diff_params: KeynomeAuthenticatorDiffParams,
+    pub stats: HashMap<Digraph, DigraphStats>,
+}
+
+impl UserProfile {
+    pub fn new(n_profile: u32, n_sample: u32, diff_base: f64,
+           _diff_params: &KeynomeAuthenticatorDiffParams,
+           _stats: &HashMap<Digraph, DigraphStats>) -> UserProfile {
+
+        let diff_params = (*_diff_params).clone();
+        let mut stats: HashMap<Digraph, DigraphStats> = HashMap::new();
+        for (k, v) in _stats.iter() {
+            stats.insert(*k, (*v).clone());
+        }
+        UserProfile { n_profile, n_sample, diff_base, diff_params, stats }
+    }
+
+    pub fn serialize(&self) -> String {
+        let mut obj = HashMap::new();
+
+        obj.insert("n_profile", format!("{}", self.n_profile));
+        obj.insert("n_sample", format!("{}", self.n_sample));
+        obj.insert("diff_base", format!("{}", self.diff_base));
+        obj.insert("diff_params", serde_json::to_string(&self.diff_params).unwrap());
+        obj.insert("stats", KeystrokeLogger::serialize_digraph_statistics(&self.stats));
+
+        serde_json::to_string(&obj).unwrap()
+    }
+
+    pub fn deserialize(serialized: &str) -> UserProfile {
+        let str_keyed_map: HashMap<String, String> = serde_json::from_str(serialized).unwrap();
+
+        let n_profile: u32 = str_keyed_map.get("n_profile").unwrap().parse().unwrap();
+        let n_sample: u32 = str_keyed_map.get("n_sample").unwrap().parse().unwrap();
+        let diff_base: f64 = str_keyed_map.get("diff_base").unwrap().parse().unwrap();
+        let diff_params: KeynomeAuthenticatorDiffParams = serde_json::from_str(
+            &str_keyed_map.get("diff_params").unwrap()).unwrap();
+        let stats: HashMap<Digraph, DigraphStats> = KeystrokeLogger::deserialize_digraph_statistics(
+            &str_keyed_map.get("stats").unwrap());
+
+        UserProfile { n_profile, n_sample, diff_base, diff_params, stats }
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -217,7 +350,7 @@ mod tests {
     }
 
     #[test]
-    fn digraph_statistics_serialization() {
+    fn keystroke_logger_digraph_statistics_serialization() {
         let keystrokes = "The most likely way for the world to be destroyed, \
                           most experts agree, is by accident.  That's where we come in \
                           we're computer professionals.  We cause accidents. \
@@ -239,5 +372,79 @@ mod tests {
         keys_orig.sort();
         keys_new.sort();
         assert_eq!(keys_orig, keys_new);
+    }
+
+    #[test]
+    fn keynome_authenticator_compute_diff_base() {
+        let mut events = VecDeque::new();
+
+        // (Profile) For all 12 instances,
+
+        // a-b digraphs
+        // diffs = [1000, 2000, 3000, 500, 1000, 1500]
+        // mean = 1500.0, std = 894.4272
+
+        // b-a digraphs
+        // diffs = [1000, 1000, 2000, 500, 2000]
+        // mean = 1300.0, std = 670.8204
+
+        // (Sample 1) For first 6 instances,
+
+        // a-b digraphs
+        // diffs = [1000, 2000, 3000]
+        // mean = 2000.0, std = 1000.0
+
+        // b-a digraphs
+        // diffs = [1000, 1000]
+        // mean = 1000.0, std = 0
+
+        // (Sample 2) For last 6 instances,
+
+        // a-b digraphs
+        // diffs = [500, 1000, 1500]
+        // mean = 1000.0, std = 500.0
+
+        // b-a digraphs
+        // diffs = [500, 2000]
+        // mean = 1250.0, std = 1060.6602
+
+        events.push_back(KeyEvent { timestamp_ms: 10000, key: 'a' });
+        events.push_back(KeyEvent { timestamp_ms: 11000, key: 'b' });
+
+        events.push_back(KeyEvent { timestamp_ms: 12000, key: 'a' });
+        events.push_back(KeyEvent { timestamp_ms: 14000, key: 'b' });
+
+        events.push_back(KeyEvent { timestamp_ms: 15000, key: 'a' });
+        events.push_back(KeyEvent { timestamp_ms: 18000, key: 'b' });
+
+        events.push_back(KeyEvent { timestamp_ms: 20000, key: 'a' });
+        events.push_back(KeyEvent { timestamp_ms: 20500, key: 'b' });
+
+        events.push_back(KeyEvent { timestamp_ms: 21000, key: 'a' });
+        events.push_back(KeyEvent { timestamp_ms: 22000, key: 'b' });
+
+        events.push_back(KeyEvent { timestamp_ms: 24000, key: 'a' });
+        events.push_back(KeyEvent { timestamp_ms: 25500, key: 'b' });
+
+        let diff_params = KeynomeAuthenticatorDiffParams {
+            dispersion: false,
+            min_instances: 1,
+            max_comparisons: 100,
+        };
+
+        // Profile <=> Sample 1
+        // (a-b) abs(1500 - 2000) = 500
+        // (b-a) abs(1300 - 1000) = 300
+        // diff = 800
+
+        // Profile <=> Sample 2
+        // (a-b) abs(1500 - 1000) = 500
+        // (b-a) abs(1300 - 1250) = 50
+        // diff = 550
+
+        // diff_base = (800 + 550) / 2 = 675
+
+        let diff_base = KeynomeAuthenticator::compute_diff_base(&events, 12, 6, &diff_params).unwrap();
+        assert_eq!(diff_base, 675.0);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,31 @@ use std::io::{stdin, Read};
 use std::io::prelude::*;
 use std::fs::File;
 use std::path::Path;
-use std::collections::HashMap;
 
 extern crate clap;
 use clap::{Arg, App, AppSettings, SubCommand};
 
 mod lib;
 use lib::KeystrokeLogger;
-use lib::{Digraph, DigraphStats};
+use lib::{KeynomeAuthenticator, KeynomeAuthenticatorDiffParams, UserProfile};
 
-fn save_user_profile(stats: &HashMap<Digraph, DigraphStats>, filename: &str) {
-    let serialized = KeystrokeLogger::serialize_digraph_statistics(stats);
-
+fn save_user_profile(profile: &UserProfile, filename: &str) {
+    let serialized = profile.serialize();
     let path = Path::new(filename);
     let mut file = File::create(&path).unwrap();
     file.write_all(serialized.as_bytes()).unwrap();
+    println!("user profile stored in {}.", filename);
 }
 
-fn load_user_profile(filename: &str) -> HashMap<Digraph, DigraphStats> {
+fn load_user_profile(filename: &str) -> UserProfile {
     let path = Path::new(filename);
     let mut file = File::open(&path).unwrap();
 
     let mut s = String::new();
     file.read_to_string(&mut s).unwrap();
 
-    let stats = KeystrokeLogger::deserialize_digraph_statistics(&s);
-    stats
+    let profile = UserProfile::deserialize(&s);
+    profile
 }
 
 fn main() {
@@ -36,19 +35,51 @@ fn main() {
     let matches = App::new("Keynome")
         .author("Minhwan Kim <azurelysium@gmail.com>")
         .about("Continuous authentication using Keystroke Dynamics")
+        .version("0.0.1")
         .setting(AppSettings::SubcommandRequiredElseHelp)
-        .arg(Arg::with_name("v")
+        .arg(Arg::with_name("verbosity")
              .short("v")
              .multiple(true)
              .help("Sets the level of verbosity"))
         .subcommand(SubCommand::with_name("profile")
                     .about("generates a user profile")
+                    .arg(Arg::with_name("n_profile")
+                         .long("n_profile")
+                         .value_name("NUMBER")
+                         .help("Sets the number of profile keyevents")
+                         .default_value("5000")
+                         .takes_value(true))
+                    .arg(Arg::with_name("n_sample")
+                         .long("n_sample")
+                         .value_name("NUMBER")
+                         .help("Sets the number of sample keyevents")
+                         .default_value("1000")
+                         .takes_value(true))
+                    .arg(Arg::with_name("min_instances")
+                         .long("min_instances")
+                         .value_name("NUMBER")
+                         .help("Sets the minimum number of digraph stats instances")
+                         .default_value("2")
+                         .takes_value(true))
+                    .arg(Arg::with_name("max_comparisons")
+                         .long("max_comparisons")
+                         .value_name("NUMBER")
+                         .help("Sets the maximum number of comparisons")
+                         .default_value("100")
+                         .takes_value(true))
+                    .arg(Arg::with_name("use_dispersion")
+                         .long("use_dispersion")
+                         .value_name("NUMBER")
+                         .help("Sets the flag for using dispersion when diff computed")
+                         .default_value("0")
+                         .takes_value(true))
                     .arg(Arg::with_name("outfile")
                          .short("o")
                          .long("outfile")
                          .value_name("FILE")
                          .help("Sets an output file where a user profile will be stored")
-                         .takes_value(true)))
+                         .takes_value(true))
+        )
         .subcommand(SubCommand::with_name("auth")
                     .about("authenticates a user using the pre-computed user profile")
                     .arg(Arg::with_name("infile")
@@ -57,22 +88,30 @@ fn main() {
                          .value_name("FILE")
                          .help("Sets an input file where a user profile is stored")
                          .required(true)
-                         .takes_value(true)))
+                         .takes_value(true))
+        )
         .get_matches();
 
-    let verbosity = matches.occurrences_of("v");
+    let verbosity = matches.occurrences_of("verbosity");
 
     // process subcommand
 
     // Subcomnad - profile
     if let Some(matches) = matches.subcommand_matches("profile") {
 
+        let n_profile: u32 = matches.value_of("n_profile").unwrap().parse().unwrap();
+        let n_sample: u32 = matches.value_of("n_sample").unwrap().parse().unwrap();
+        let min_instances: u32 = matches.value_of("min_instances").unwrap().parse().unwrap();
+        let max_comparisons: u32 = matches.value_of("max_comparisons").unwrap().parse().unwrap();
+        let use_dispersion: u32 = matches.value_of("use_dispersion").unwrap().parse().unwrap();
+
         println!("Press ! key to stop recording keystrokes");
 
         let mut kstr = KeystrokeLogger::new();
-        kstr.set_events_limit(5000);
+        kstr.set_events_limit(n_profile as usize);
 
         // read user keystrokes from Stdin character by character
+        let mut cnt_newline = 0;
         let mut buf = [0];
         while let Ok(_) = stdin().read(&mut buf) {
             let ch = buf[0] as char;
@@ -85,6 +124,12 @@ fn main() {
             } else if ch.is_ascii_alphabetic() {
                 kstr.add_keystroke(ch);
             }
+
+            // if shelljacked-terminal is closed, newline is typed infinitely
+            cnt_newline = if ch == '\n' { cnt_newline + 1 } else { 0 };
+            if cnt_newline > 10 {
+                break;
+            }
         }
 
         // compute statistics and serialize this
@@ -95,14 +140,29 @@ fn main() {
             }
         }
 
+        // compute inherent difference level
+        let diff_params = KeynomeAuthenticatorDiffParams {
+            dispersion: if use_dispersion == 1 { true } else { false },
+            min_instances,
+            max_comparisons,
+        };
+
+        let events = kstr.get_key_events();
+        let diff_base = KeynomeAuthenticator::compute_diff_base(events, 12, 6, &diff_params).unwrap();
+
         // save a user profile
+        let profile = UserProfile::new(n_profile, n_sample, diff_base, &diff_params, &stats);
         let filename = matches.value_of("outfile").unwrap_or("profile.json");
-        save_user_profile(&stats, &filename);
+        save_user_profile(&profile, &filename);
     }
 
     // Subcomnad - auth
     if let Some(matches) = matches.subcommand_matches("auth") {
         let filename = matches.value_of("infile").unwrap();
-        let stats_profile = load_user_profile(&filename);
+        let profile = load_user_profile(&filename);
+        println!("n_profile: {}", profile.n_profile);
+        println!("n_sample: {}", profile.n_sample);
+        println!("diff_base: {}", profile.diff_base);
+        println!("diff_params: {:?}", profile.diff_params);
     }
 }
